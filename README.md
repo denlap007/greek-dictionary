d61a52892f05bb59755b922873b77c8e56a39a97
Dionysis Lappas <lappas@intralot.com>
2022-01-15 20:37:15 +0200

    repo initialization

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# greek-dictionary
-Telegram Bot for greek dictionary
+# greek-dictionary-bot
+Telegram Bot for greek dictionary, using as source https://www.greek-language.gr
+
+## Contents
+
+- [greek-dictionary-bot](#greek-dictionary-bot)
+  - [Contents](#contents)
+  - [Requirements](#requirements)
+  - [Installation](#installation)
+  - [Quickstart](#quickstart)
+  - [Docker](#docker)
+      - [Run docker container](#run-docker-container)
+  - [License](#license)
+
+## Requirements
+
+Node.js **v16** or higher
+
+## Installation
+
+```js
+npm install
+```
+
+## Quickstart
+
+```js
+npm start
+```
+
+## Docker
+
+The application is dockerized and can be run by starting a container.
+
+#### Run docker container
+
+Set the BOT_TOKEN environment variable at runtime
+
+```sh
+docker run -d --name greek-dictionary-bot -e "BOT_TOKEN=<THE BOT TOKEN>" denlap/greek-dictionary-bot
+```
+
+## License
+
+Licensed under [GPL v3.0]
+
+[gpl v3.0]: https://hapi.dev/
+[localhost:3000/api-doc]: http://localhost:3000/api-doc
---
+++ bot.js
@@ -0,0 +1,56 @@
+import got from "got";
+import TelegramBot from "node-telegram-bot-api";
+import cheerio from "cheerio";
+
+// The Telegram token you received from @BotFather
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const DICTIONARY_URL =
+  "https://www.greek-language.gr/greekLang/modern_greek/tools/lexica/triantafyllides/search.html";
+const MAX_STRING_SIZE = 4096;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+
+// Matches everythins
+bot.onText(/(.+)/, async (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const word = match[1]; // the captured text
+
+  try {
+    const { body } = await request(word);
+    let trans = extractLemma(body);
+
+    // check msg size cause library has limits and break msg in multiple messages
+    const numOfMessages = Math.ceil(trans.length / MAX_STRING_SIZE);
+
+    for (let i = 1; i <= numOfMessages; i++) {
+      const start = (i - 1) * MAX_STRING_SIZE;
+      const end = i * MAX_STRING_SIZE;
+      let part = trans.substring(start, end);
+      bot.sendMessage(chatId, part);
+    }
+
+    console.log("Searched: ", word);
+  } catch (error) {
+    console.error("Error: ", error.message);
+  }
+});
+
+const extractLemma = html => {
+  const $ = cheerio.load(html);
+  const trans = $("#lemmas").text();
+  return trans ? trans : "Δε βρέθηκε αποτέλεσμα";
+};
+
+const request = async word => {
+  return got({
+    url: DICTIONARY_URL,
+    searchParams: {
+      lq: word,
+    },
+  });
+};
